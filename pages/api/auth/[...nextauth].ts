2acbc627d2941dd9a5ec5d3cf9f92604a9542172
BlueSky428 <bluesky.engineer428@gmail.com>
2022-08-19 23:23:01 -0400

    POC: can logout of server session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import axios from "axios";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import querystring from 'query-string';
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -7,63 +9,6 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET,
-    // }),
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
-    // }),
-    // TwitterProvider({
-    //   clientId: process.env.TWITTER_ID,
-    //   clientSecret: process.env.TWITTER_SECRET,
-    // }),
-    // Auth0Provider({
-    //   clientId: process.env.AUTH0_ID,
-    //   clientSecret: process.env.AUTH0_SECRET,
-    //   issuer: process.env.AUTH0_ISSUER,
-    // }),
-    // {
-    //   id: "MyKeycloak",
-    //   name: "MyKeycloak",
-    //   type: "oauth",
-    //   wellKnown: "http://localhost:3255/auth/realms/SpecialRealm/.well-known/openid-configuration",
-    //   authorization: { params: { scope: "openid email profile" } },
-    //   idToken: true,
-    //   checks: ["pkce"],
-    //   clientId: "recipe_management.nextjs",
-    //   clientSecret: "974d6f71-d41b-4601-9a7a-a33081f80622",
-    //   profile(profile) {
-    //     return {
-    //       id: profile.sub,
-    //       name: profile.name,
-    //       email: profile.email,
-    //       image: profile.picture,
-    //     }
-    //   },
-    // }
     {
       id: "myKey",
       name: "Paul's Key",
@@ -84,15 +29,98 @@ export const authOptions: NextAuthOptions = {
       },
     }
   ],
+  events: {
+    async signOut({token}) {
+      var refreshToken = token.refreshToken
+      let headers = { "Content-Type": "application/x-www-form-urlencoded" };
+      try {
+        await axios.post("http://localhost:3255/auth/realms/DevRealm/protocol/openid-connect/logout",
+        querystring.stringify({
+          refresh_token: refreshToken,
+          client_secret: '974d6f71-d41b-4601-9a7a-a33081f82188',
+          client_id: 'recipe_management.next',
+      }),
+          { headers },
+        );
+      } catch (e) { }
+    },
+  },
   theme: {
     colorScheme: "light",
   },
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        }
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.user = token.user
+      session.accessToken = token.accessToken
+      session.error = token.error
+
+      return session
     },
   },
+}
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+ async function refreshAccessToken(token) {
+  try {
+    const url =
+      "http://localhost:3255/auth/realms/DevRealm/protocol/openid-connect/token?" +
+      new URLSearchParams({
+        client_secret: '974d6f71-d41b-4601-9a7a-a33081f82188',
+        client_id: 'recipe_management.next',
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      })
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
 }
 
 export default NextAuth(authOptions)
